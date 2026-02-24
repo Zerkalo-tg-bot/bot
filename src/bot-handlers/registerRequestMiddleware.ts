dbd5055e7db1d2a2c9bdfa7529bd03974922d343
Vladislav Vagner <vladislawwagner@gmail.com>
2026-02-24 09:27:04 +0100

    refactored request middleware

--- src/bot-handlers/registerRequestMiddleware.ts
+++ src/bot-handlers/registerRequestMiddleware.ts
@@ -1,16 +1,17 @@
 import type { Telegraf } from "telegraf";
 import { canRequest } from "../api/http/canRequest.js";
+import { sendLocalizedStaticMessage } from "../i18n/sendLocalizedStaticMessage.js";
+import { ELanguage } from "../core/enums/language.enum.js";
 
 export function registerRequestMiddleware(bot: Telegraf) {
   bot.use(async (ctx, next) => {
-    console.log(`Received update of type ${ctx.updateType} from user ${ctx.from?.id}`);
     const userId = ctx.from?.id;
     if (!userId) {
       return;
     }
 
     if (!canRequest(userId)) {
-      await ctx.reply("You are sending requests too quickly. Please wait a moment.");
+      await sendLocalizedStaticMessage(ctx, "error-section.you_are_sending_messages_too_frequently", ELanguage.ENGLISH);
       return;
     }
 
