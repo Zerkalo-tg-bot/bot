5f1e2c63936bd408062a6d0b9cc3705657df6950
Vladislav Vagner <vladislawwagner@gmail.com>
2026-02-24 04:19:37 +0100

    optimazied withTyping tool with error handling and added command filtering on text hadler

--- src/bot-handlers/registerOnTextHandler.ts
+++ src/bot-handlers/registerOnTextHandler.ts
@@ -8,47 +8,49 @@ import i18n from "../i18n/i18n.js";
 import { ELanguage } from "../core/enums/index.js";
 
 export function registerOnTextHandler(bot: Telegraf) {
-  bot.on("message", async (ctx) => {
-    await withTyping(ctx, async () => {
-      try {
-        const user = await userService.getUser(ctx.message.from.id).catch((error) => {
-          console.error("Error getting user:", error);
-          throw error;
-        });
-
-        if (!user.acceptedDisclaimer) {
-          const disclaimerText = i18n.t("info-section.disclaimer", { lng: user.language });
-          const continueLabel = i18n.t("info-section.continue_button", { lng: user.language });
-
-          await ctx.reply(disclaimerText, {
-            parse_mode: "HTML",
-            reply_markup: {
-              inline_keyboard: [[{ text: continueLabel, callback_data: "accept_disclaimer" }]],
-            },
+  bot.on("text", async (ctx) => {
+    if (ctx.message.text.startsWith("/")) {
+      await sendLocalizedStaticMessage(ctx, "error-section.this_command_does_not_exist", ELanguage.ENGLISH);
+      return;
+    }
+    if (ctx.message)
+      await withTyping(ctx, async () => {
+        try {
+          const user = await userService.getUser(ctx.message.from.id).catch((error) => {
+            console.error("Error getting user:", error);
+            throw error;
           });
-          return;
+
+          if (!user.acceptedDisclaimer) {
+            const disclaimerText = i18n.t("info-section.disclaimer", { lng: user.language });
+            const continueLabel = i18n.t("info-section.continue_button", { lng: user.language });
+
+            await ctx.reply(disclaimerText, {
+              parse_mode: "HTML",
+              reply_markup: {
+                inline_keyboard: [[{ text: continueLabel, callback_data: "accept_disclaimer" }]],
+              },
+            });
+            return;
+          }
+
+          const msg = ctx.message;
+
+          if (msg.text.length > 1000) {
+            await sendLocalizedStaticMessage(ctx, "error-section.message_too_long_max_length_is_1000_characters", user.language);
+            return;
+          }
+
+          const clientMessage: IMessage = { content: msg.text };
+          const response = await messageService.sendMessage(ctx.message.from.id, clientMessage).catch((error) => {
+            console.error("Error sending message to API:", error);
+            throw error;
+          });
+
+          await ctx.reply(response.content);
+        } catch {
+          await sendLocalizedStaticMessage(ctx, "error-section.error_sending_message_please_try_again_later", ELanguage.ENGLISH);
         }
-
-        const msg = ctx.message;
-        if (!("text" in msg) || typeof msg.text !== "string") {
-          return;
-        }
-
-        if (msg.text.length > 1000) {
-          await sendLocalizedStaticMessage(ctx, "error-section.message_too_long_max_length_is_1000_characters", user.language);
-          return;
-        }
-
-        const clientMessage: IMessage = { content: msg.text };
-        const response = await messageService.sendMessage(ctx.message.from.id, clientMessage).catch((error) => {
-          console.error("Error sending message to API:", error);
-          throw error;
-        });
-
-        await ctx.reply(response.content);
-      } catch {
-        await sendLocalizedStaticMessage(ctx, "error-section.error_sending_message_please_try_again_later", ELanguage.ENGLISH);
-      }
-    });
+      });
   });
 }
--- src/core/telegram/withTyping.ts
+++ src/core/telegram/withTyping.ts
@@ -1,18 +1,27 @@
 import type { Context } from "telegraf";
+import { sendLocalizedStaticMessage } from "../../i18n/sendLocalizedStaticMessage.js";
+import { ELanguage } from "../enums/language.enum.js";
 
 type TypingCapableContext = {
   sendChatAction: Context["sendChatAction"];
 };
 
-export async function withTyping<T>(ctx: TypingCapableContext, work: () => Promise<T>, intervalMs = 5000): Promise<T> {
-  ctx.sendChatAction("typing");
-  const sendActionIntervalId = setInterval(() => {
-    ctx.sendChatAction("typing");
+export async function withTyping<T>(ctx: Context, work: () => Promise<T>, intervalMs = 5000): Promise<T> {
+  await sendTypingAction(ctx);
+  const sendActionIntervalId = setInterval(async () => {
+    await sendTypingAction(ctx);
   }, intervalMs);
-
   try {
     return await work();
   } finally {
     clearInterval(sendActionIntervalId);
+  }
+}
+
+async function sendTypingAction(ctx: Context) {
+  try {
+    await ctx.sendChatAction("typing");
+  } catch (error) {
+    console.error("Error sending typing action:", error);
   }
 }
