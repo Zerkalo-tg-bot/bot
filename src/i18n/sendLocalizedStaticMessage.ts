4e8e41a1999c3c34ef55db595821f612ca33c9b0
Vladislav Vagner <vladislawwagner@gmail.com>
2026-02-24 12:52:49 +0100

    added error handling while sending localized message

--- src/i18n/sendLocalizedStaticMessage.ts
+++ src/i18n/sendLocalizedStaticMessage.ts
@@ -3,5 +3,9 @@ import type { ELanguage } from "../core/enums/index.js";
 import i18n from "./i18n.js";
 
 export const sendLocalizedStaticMessage = (ctx: Context, messageKey: string, lng: ELanguage) => {
-  return ctx.reply(i18n.t(messageKey, { lng }));
+  try {
+    return ctx.reply(i18n.t(messageKey, { lng }));
+  } catch (error) {
+    console.error("Error sending localized static message:", error);
+  }
 };
